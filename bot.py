7161997c86850dd990584f1f0a1c312e2f8724bc
Hoixi <furkan.kayali.1905@gmail.com>
2020-02-26 20:38:33 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,9 +9,10 @@ bot = commands.Bot(command_prefix=bot_prefix)
 
 @bot.event
 async def on_ready(user):
+    serverr = bot.get_server('672019759729999884')
     channel = bot.get_channel('682247713756020857')  
     message = await bot.send_message(channel, "React to me!")
-    role = discord.utils.get(user.server.roles, name="Offical-Waifu-Hunt")
+    role = discord.utils.get(serverr.roles, name="Offical-Waifu-Hunt")
     while True:
         reaction = await bot.wait_for_reaction(emoji=":heart:", message=message)
         await bot.add_roles(reaction.message.author, role)
