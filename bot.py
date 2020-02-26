c008252155fc1863d6902782381138a8fc5d3cb5
Hoixi <furkan.kayali.1905@gmail.com>
2020-02-26 22:35:45 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,14 +8,12 @@ bot_prefix="!"
 bot = commands.Bot(command_prefix=bot_prefix)
 
 @bot.event
-async def on_ready():
-    yummy = discord.utils.find(lambda g : g.id == '672019759729999884' , bot.servers)
-    channel = bot.get_channel('682247713756020857')  
-    message = await bot.send_message(channel, "Kuralları okuyup kabul ettiyseniz lütfen aşağıda ki onay simgesine tıklayınız.")    
-    role = discord.utils.get(yummy.roles, name="annen")
-    while True:
-        reaction = await bot.wait_for_reaction(emoji="🏃", message=message)
-        await bot.send_message(channel, "Kayıdınız Tamamlandı!")       
-        await reaction.message.author.add_roles(role)
+async def on_reaction_add(reaction,user):
+    channel = reaction.message.channel
+
+    if (channel == "682247713756020857"):
+        yummy = discord.utils.find(lambda g : g.id == '672019759729999884' , bot.servers)
+        role = discord.utils.get(yummy.roles, name="annen")
+        await bot.add_roles(user.author, role)
 
 bot.run(os.environ.get('token'))
