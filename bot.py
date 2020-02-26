0592533515eac0c226b98afa2128e762bd23114b
Hoixi <furkan.kayali.1905@gmail.com>
2020-02-26 22:03:57 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ bot = commands.Bot(command_prefix=bot_prefix)
 async def on_ready():
     yummy = discord.utils.find(lambda g : g.id == '672019759729999884' , bot.servers)
     channel = bot.get_channel('682247713756020857')  
-    messagee = await bot.send_message(channel, "Kuralları okuyup kabul ettiyseniz lütfen aşağıda ki onay simgesine tıklayınız.")    
+    message = await bot.send_message(channel, "Kuralları okuyup kabul ettiyseniz lütfen aşağıda ki onay simgesine tıklayınız.")    
     role = discord.utils.get(yummy.roles, name="annen")
     while True:
         reaction = await bot.wait_for_reaction(emoji="🏃", message=message)
