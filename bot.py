9d0ae6a9496ae1dec286ac479b8fa79c471ccf51
Hoixi <furkan.kayali.1905@gmail.com>
2020-02-26 21:34:26 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,10 +12,10 @@ async def on_ready():
     serverr = bot.get_server('672019759729999884')
     channel = bot.get_channel('682247713756020857')  
     message = await bot.send_message(channel, "Kuralları okuyup kabul ettiyseniz lütfen aşağıda ki onay simgesine tıklayınız.")    
+    role = discord.utils.get(channel.server.roles, name="annen")
     while True:
         reaction = await bot.wait_for_reaction(emoji="🏃", message=message)
-        await bot.send_message(channel, "Kayıdınız Tamamlandı!")
-        role = discord.utils.get(reaction.message.author.server.roles, name="annen")
+        await bot.send_message(channel, "Kayıdınız Tamamlandı!")       
         await bot.add_roles(reaction.message.author, role)
 
 bot.run(os.environ.get('token'))
