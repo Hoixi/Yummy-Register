614658e51a98f09f491ca264d1580ab5e6a40d68
Hoixi <furkan.kayali.1905@gmail.com>
2020-02-27 21:49:51 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,5 +13,11 @@ async def on_reaction_add(reaction,user,server):
     role = discord.utils.get(server.roles, name="annen")
     await bot.add_roles(user.author, role)
     await bot.send_message(channel , "Vay aq")
+    
+@bot.command(pass_context=True)
+async def yum(ctx):
+    role = discord.utils.get(ctx.server.roles, name="annen")
+    await bot.add_role(ctx.author , role)
+    
 
 bot.run(os.environ.get('token'))
