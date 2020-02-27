205929e219663c06e9ee30e8cf4287c801a9528a
Hoixi <furkan.kayali.1905@gmail.com>
2020-02-28 00:41:21 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,14 +8,18 @@ client = discord.Client()
 @client.event
 async def on_raw_reaction_add(peyload):
     message_id = peyload.message_id
-    if message_id == 682667145787867393:
+    if message_id == 682696747126227110:
         guild_id = peyload.guild_id
         guild = discord.utils.find(lambda g : g.id == guild_id, client.guilds)
-        role = discord.utils.get(guild.roles, name='annen')
+        role = discord.utils.get(guild.roles, name='Approved Collector🦉')
         if role is not None:
             member = discord.utils.find(lambda m : m.id == peyload.user_id, guild.members)
             if member is not None:
                 await member.add_roles(role)
+                channel = client.get_channel(682692982561964054)
+                await channel.send( '<@%s> <#682212655783477364> kanalına hoş geldin.<#681960108426657933> kanalındaki bilgileri okumayı unutmayın. İyi eğlenceler :)' % peyload.user_id)
+                user = client.get_user( peyload.user_id)
+                await user.send(" ```Waifu Hunt Komutları \n $w = Animelerden Waifu verir \n $wg = Oyunlardan Waifu verir \n $im = Waifu bilgisi verir \n $mm = Hareminizi görüntüler \n $mu = Hareme ekleme hakkı takibi```")
             else:
                 print("Kullanıcı Yok")
         else:
@@ -24,10 +28,10 @@ async def on_raw_reaction_add(peyload):
 @client.event
 async def on_raw_reaction_remove(peyload):
     message_id = peyload.message_id
-    if message_id == 682667145787867393:
+    if message_id == 682696747126227110:
         guild_id = peyload.guild_id
         guild = discord.utils.find(lambda g : g.id == guild_id, client.guilds)
-        role = discord.utils.get(guild.roles, name='annen')
+        role = discord.utils.get(guild.roles, name='Approved Collector🦉')
         if role is not None:
             member = discord.utils.find(lambda m : m.id == peyload.user_id, guild.members)
             if member is not None:
@@ -35,7 +39,7 @@ async def on_raw_reaction_remove(peyload):
             else:
                 print("Kullanıcı Yok")
         else:
-            print("Rol Yok")
+            print("Rol Yok")  
     
 
 client.run(os.environ.get('token'))
